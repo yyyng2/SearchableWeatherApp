0edcb9e5f6c0731170738dab2b08f891467a7f0e
yyyng2 <yyyng2@gmail.com>
2023-01-11 00:12:26 +0900

    Initial Commit

---
+++ SearchableWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchableWeatherApp
+//
+//  Created by Y on 2023/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
